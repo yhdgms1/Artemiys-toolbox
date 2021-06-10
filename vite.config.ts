8e80142be61916520dc7adab9e6c1d5101feba0a
Artemis69 <shuckinart@yandex.ru>
2021-06-10 19:35:23 +0300

    make it better

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { minifyHtml } from 'vite-plugin-html'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import { default as solidPlugin } from 'vite-plugin-solid'
-import { default as linariaPlugin } from 'vite-plugin-linaria'
 import { default as legacyPlugin } from '@vitejs/plugin-legacy'
 
 const DEV = process.env.MODE === 'dev'
@@ -11,11 +10,11 @@ export default defineConfig({
   plugins: [
     solidPlugin(),
     vanillaExtractPlugin(),
-    linariaPlugin(),
-    legacyPlugin({
+    !DEV && legacyPlugin({
       targets: ['chrome 75', 'firefox 78', 'not IE 11'],
+      renderLegacyChunks: false
     }),
-    minifyHtml({}),
+    !DEV && minifyHtml(),
   ],
   build: {
     target: ['es2020'],
