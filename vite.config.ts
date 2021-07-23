de6c4384476d4f17380b270389546f6b6eac4a7f
Artem Schukin <shuckinart@yandex.ru>
2021-07-23 22:04:09 +0300

    remove babel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite'
 import { minifyHtml } from 'vite-plugin-html'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import { babelifyPlugin } from './plugins/babelifyPlugin'
 import { default as solidPlugin } from 'vite-plugin-solid'
 
 const DEV = process.env.MODE === 'dev'
 
 export default defineConfig({
   plugins: [
-    solidPlugin(),
+    solidPlugin({
+      babel: {
+        babelrc: false,
+      },
+    }),
     vanillaExtractPlugin(),
     !DEV && minifyHtml(),
-    !DEV && babelifyPlugin(),
   ],
   build: {
     cssCodeSplit: false,
-    target: ['es2020'],
+    target: ['chrome64'], //* never add 'browserlist' in 'package.json'. and do not add something else here. 'require' will appear in browser build
   },
 })
