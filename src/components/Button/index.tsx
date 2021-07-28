c51d6f3365492e36605c8cc235052abc32558e69
Artem Schukin <shuckinart@yandex.ru>
2021-07-28 16:24:59 +0300

    fix copy in old browsers

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,6 +2,7 @@ import type { JSX } from 'solid-js'
 import { createSignal } from 'solid-js'
 import { t } from '../../i18n'
 import clsx from 'clsx'
+import copy from 'copy-text-to-clipboard'
 import * as styles from './style.css'
 
 export const Button = (props: JSX.ButtonHTMLAttributes<HTMLButtonElement>) => (
@@ -18,21 +19,18 @@ export const CopyButton = (props: CopyButtonProps) => {
   const [text, setText] = createSignal(t(['btn__copy', 'default']))
 
   const clickHandler = () => {
-    try {
-      navigator.clipboard.writeText(props.copy || '').then(() => {
-        setText(t(['btn__copy', 'active']))
-        const timeout = setTimeout(() => {
-          setText(t(['btn__copy', 'default']))
-          clearTimeout(timeout)
-        }, 750)
-      })
-    } catch (_) {
+    const copied = copy(props.copy || '')
+
+    if(copied){
+      setText(t(['btn__copy', 'active']))
+    }else{
       setText(t(['btn__copy', 'err']))
-      const timeout = setTimeout(() => {
-        setText(t(['btn__copy', 'default']))
-        clearTimeout(timeout)
-      }, 750)
     }
+
+    const timeout = setTimeout(() => {
+      setText(t(['btn__copy', 'default']))
+      clearTimeout(timeout)
+    }, 750)
   }
 
   return (
