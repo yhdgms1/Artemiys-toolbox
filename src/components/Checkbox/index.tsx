194449b2a2042b4cdaa297fb105e5ddf05ebe230
Artem Schukin <shuckinart@yandex.ru>
2021-08-03 23:44:50 +0300

    Makes Checkbox looks better, return to vite 2.4.4

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,7 +3,9 @@ import * as styles from './style.css'
 
 export const Checkbox = (props: JSX.InputHTMLAttributes<HTMLInputElement>) => (
   <div class={styles.checkbox}>
-    <input type="checkbox" name="is_private" {...props} class={styles.input}/>
-    <label class={styles.label} for={props.id}>{props.children}</label>
+    <input type="checkbox" name="is_private" {...props} class={styles.input} />
+    <label class={styles.label} for={props.id}>
+      {props.children}
+    </label>
   </div>
 )
--- src/components/Checkbox/style.css.ts
+++ src/components/Checkbox/style.css.ts
@@ -1,16 +1,13 @@
-import { style, globalStyle } from '@vanilla-extract/css'
+import { style } from '@vanilla-extract/css'
 
 export const checkbox = style({
-  margin: '.5rem'
+  margin: '.5rem',
 })
 
 export const input = style({
   position: 'absolute',
-  zIndex: '-1',
+  zIndex: 0 - 1,
   opacity: 0,
-  selectors: {
-    
-  }
 })
 
 export const label = style({
@@ -33,37 +30,34 @@ export const label = style({
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center center',
       backgroundSize: '100%',
-      "@media": {
+      '@media': {
         '(prefers-color-scheme: dark)': {
           borderColor: '#393939',
-        }
-      }
+          backgroundColor: '#191919'
+        },
+      },
     },
     [`${input}:not(:disabled):not(:checked) + &:hover::before`]: {
-      borderColor: '#e69ab0'
+      borderColor: '#e69ab0',
     },
     [`${input}:not(:disabled):active + &::before`]: {
-      backgroundColor: '#ffc2d4',
-      borderColor: '#e69ab0'
-    },
-    [`${input}:focus + &::before`]: {
-      boxShadow: `0 0 0 .125rem #e69ab0`,
-      "@media": {
-        '(prefers-color-scheme: dark)': {
-          boxShadow: `0 0 0 .125rem #DB6D8C`
-        }
-      }
+      backgroundColor: '#e69ab0',
+      borderColor: '#e69ab0',
     },
     [`${input}:focus:not(:checked) + &::before`]: {
-      borderColor: '#e69ab0'
-    },
-    [`${input}:checked + &::before`]:{
       borderColor: '#e69ab0',
-      backgroundColor: '#FF6291',
-      backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'%3E%3Cpath stroke='white' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M5.75 12.8665L8.33995 16.4138C9.15171 17.5256 10.8179 17.504 11.6006 16.3715L18.25 6.75'%3E%3C/path%3E%3C/svg%3E%0A")`
+    },
+    [`${input}:checked + &::before`]: {
+      borderColor: '#e69ab0',
+      backgroundColor: '#DB6D8C',
+      backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'%3E%3Cpath stroke='white' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M5.75 12.8665L8.33995 16.4138C9.15171 17.5256 10.8179 17.504 11.6006 16.3715L18.25 6.75'%3E%3C/path%3E%3C/svg%3E%0A")`,
+    },
+    [`${input}:focus + &::before`]: {
+      borderColor: '#DB6D8C',
+      boxShadow: `0 0 0 .063em #DB6D8C`
     },
     [`${input}:disabled + &::before`]: {
-      backgroundColor: '#e9ecef'
-    }
+      backgroundColor: '#e9ecef',
+    },
   },
 })
