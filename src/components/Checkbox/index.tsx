18ed2403a406e6c5de0708d330d8c6785306ae59
Artem Schukin <shuckinart@yandex.ru>
2021-08-06 20:49:07 +0300

    fix: checkbox name attribute

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,7 +3,7 @@ import * as styles from './style.css'
 
 export const Checkbox = (props: JSX.InputHTMLAttributes<HTMLInputElement>) => (
   <div class={styles.checkbox}>
-    <input type="checkbox" name="is_private" {...props} class={styles.input} />
+    <input type="checkbox" {...props} class={styles.input} />
     <label class={styles.label} for={props.id}>
       {props.children}
     </label>
