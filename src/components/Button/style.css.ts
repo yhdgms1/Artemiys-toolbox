d47484692d34854f8130f8653f933f5fe0e188e0
Artem Schukin <shuckinart@yandex.ru>
2021-08-11 16:16:51 +0300

    fix: button text jump

--- src/components/Button/style.css.ts
+++ src/components/Button/style.css.ts
@@ -11,13 +11,19 @@ export const button = style({
   background: '#f5f5f5',
   color: '#323130',
   transform: `scale(1)`,
-  transition: `all .2s cubic-bezier(.4,0,.2,1)`,
+  transition: `transform .2s cubic-bezier(.4,0,.2,1)`,
   textRendering: 'optimizeLegibility',
   ':hover': {
     filter: `drop-shadow(.125rem .125rem .313rem #1c1c1c30)`,
   },
   ':active': {
     transform: `scale(.97)`,
+  },
+  '::before': {
+    content: `''`,
+    display: 'inline-block',
+    height: '2.7rem',
+    verticalAlign: 'middle'
   },
   '@media': {
     '(prefers-color-scheme: dark)': {
