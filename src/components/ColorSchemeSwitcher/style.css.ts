34f6451636e75b43040996298aab79170d532eec
Artemiy Schukin <shuckinart@yandex.ru>
2021-09-28 21:01:22 +0300

    fix: chrome 75 compatibility

--- src/components/ColorSchemeSwitcher/style.css.ts
+++ src/components/ColorSchemeSwitcher/style.css.ts
@@ -19,14 +19,20 @@ export const fieldset = style({
 
 export const dialog = style({
   position: 'fixed',
-  inset: 0,
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
   zIndex: dialogZIndex,
   overflowY: 'auto',
 })
 
 export const dialogOverlay = style({
   position: 'fixed',
-  inset: 0,
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
   background: '#00000095',
 })
 
