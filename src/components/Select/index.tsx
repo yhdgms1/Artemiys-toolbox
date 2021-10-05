b620a1efb7d09cf879a60b0013dce83021fe8ffe
Artemiy Schukin <shuckinart@yandex.ru>
2021-10-05 18:34:26 +0300

    fix: extra attribute

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,5 +1,6 @@
 import type { JSX, Component } from 'solid-js'
 
+import { splitProps } from 'solid-js'
 import * as styles from './style.css'
 import clsx from 'clsx'
 
@@ -10,7 +11,10 @@ interface Props extends JSX.SelectHTMLAttributes<HTMLSelectElement> {
 export const Select: Component<Props> = props => (
   <label class={styles.label}>
     <span class={styles.title}>{props.title}</span>
-    <select {...props} class={clsx(styles.select, props.selectClassName)}>
+    <select
+      {...splitProps(props, ['selectclassname'])[0]}
+      class={clsx(styles.select, props.selectClassName)}
+    >
       {props.children}
     </select>
   </label>
