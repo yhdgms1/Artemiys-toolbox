b836bca784617c7cab342157c0176e47921feeca
Artemiy Schukin <shuckinart@yandex.ru>
2022-03-03 19:11:10 +0300

    remove unused thing

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,11 +1,4 @@
 import { render } from 'solid-js/web'
 import { StartClient } from 'solid-start/components'
-import { ColorSchemeProvider } from '~/components/ColorSchemeSwitcher/context'
-
-const App = () => (
-  <ColorSchemeProvider>
-    <StartClient />
-  </ColorSchemeProvider>
-)
 
 render(() => <StartClient />, document.body)
