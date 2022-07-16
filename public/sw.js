9d74a4a682d052eb38b98ac39f2a9779067d6dfe
Artemiy Schukin <shuckinart@yandex.ru>
2022-07-16 16:38:42 +0300

    Unregister SW

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+/**
+ * Unregister SW
+ * @see https://github.com/NekR/self-destroying-sw
+ */
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  self.registration.unregister()
+    .then(() => {
+      return self.clients.matchAll();
+    })
+    .then((clients) => {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
